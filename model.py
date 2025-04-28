e05397de8f0e12be1e476a71643ce86fc8aae2a8
bernardonunes-hotmart <bernardodonascimentonunes@gmail.com>
2025-04-28 16:26:53 -0300

    Commit 9: Replacing conditional with polymorphism - part 2.

--- model.py
+++ model.py
@@ -23,64 +23,17 @@ class Book:
 
     def __init__(self, title: str, price_code: int):
         self.title = title
-        self.price_code = price_code
+        self.price = self.create_price(price_code)
+    
+    def create_price(self, price_code: int):  
+        if price_code == Book.NEW_RELEASE:
+            return NewReleasePrice()
+        elif price_code == Book.CHILDREN:
+            return ChildrenPrice()
+        return RegulaPrice()
+    
+    def get_charge(self, days_rented: int):
+        return self.price.get_charge(days_rented)
 
-    def get_charge(self, days_rented: int) -> float:
-        amount = 0
-        if self.price_code == Book.REGULAR:
-            amount += 2
-            if days_rented > 2:
-                amount += (days_rented - 2) * 1.5
-        elif self.price_code == Book.NEW_RELEASE:
-            amount += days_rented * 3
-        elif self.price_code == Book.CHILDREN:
-            amount += 1.5
-            if days_rented > 3:
-                amount += (days_rented - 3) * 1.5
-        return amount
-
-    def get_frequent_renter_points(self, days_rented: int) -> int:
-        points = 1
-        if self.price_code == Book.NEW_RELEASE and days_rented > 1:
-            points += 1
-        return points
-
-
-class Rental:
-    def __init__(self, book: Book, days_rented: int):
-        self.book = book
-        self.days_rented = days_rented
-
-    def get_charge(self) -> float:
-        return self.book.get_charge(self.days_rented)
-
-    def get_frequent_renter_points(self) -> int:
-        return self.book.get_frequent_renter_points(self.days_rented)
-
-
-class Client:
-
-    def __init__(self, name: str):
-        self.name = name
-        self.rentals = []
-
-    def add_rental(self, rental: Rental):
-        self.rentals.append(rental)
-
-    def statement(self) -> str:
-        total_amount = 0
-        frequent_renter_points = 0
-        result = f"Rental summary for {self.name}\n"
-        
-        for rental in self.rentals:
-            amount = rental.get_charge()
-            
-            frequent_renter_points += rental.get_frequent_renter_points()
-            
-            result += f"- {rental.book.title}: {amount}\n"
-            total_amount += amount
-        
-        # show total result
-        result += f"Total: {total_amount}\n"
-        result += f"Points: {frequent_renter_points}"
-        return result
+    def get_frequent_renter_points(self, days_rented: int):
+        return self.price.get_frequent_renter_points(days_rented)
